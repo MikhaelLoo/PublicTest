ae6f26a567cee99e8f23ed65fe84113cb87a5425
MikhaelLoo <mloo@utk.edu>
2020-04-21 15:39:16 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
  <h1>Let's see what we are working with...</h1>
+  I'm not so certain what the inner loop might be.
  <?php phpinfo(); ?>
  </body>
 </html>
