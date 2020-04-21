9d6448b5fc52118b73eff7f6ff593bf2a1ea0656
MikhaelLoo <mloo@utk.edu>
2020-04-21 15:21:37 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ <h1>Let's see what we are working with...</h1>
+ <?php phpinfo(); ?>
+ </body>
+</html>
