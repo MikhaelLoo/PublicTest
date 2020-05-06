a1d210d00ace0b7493aaae7e569f310f65d54858
Mikhael Loo <mloo@gmail.com>
2020-05-06 17:11:58 -0400

    More text

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
  <?php echo '<p>Hello World</p>'; ?> 
  <h1>Let's see what we are working with...</h1>
   I'm not so certain what the inner loop might be, but we are getting closer all the time.
+  Here is another test from VS Code to GitHub to OpenShift.
  <?php phpinfo(); ?>
  </body>
 </html>
