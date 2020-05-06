e7af0953dec5535b34c58aa6cb60a485ee395aad
Mikhael Loo <mloo@gmail.com>
2020-05-06 17:19:00 -0400

    Testing Command Line

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
  <?php echo '<p>Hello World</p>'; ?> 
  <h1>Let's see what we are working with...</h1>
   I'm not so certain what the inner loop might be, but we are getting closer all the time.
-  Here is another test from VS Code to GitHub to OpenShift.
+  Here is another test from VS Code to GitHub to OpenShift. <br/>
+  This time via commandline.
  <?php phpinfo(); ?>
  </body>
 </html>
