d6e3e0ec1237e27961d28c341238859f17cc4447
Mikhael Loo <mloo@utk.edu>
2020-05-08 10:15:51 -0400

    These changes read an INI file.

--- index.php
+++ index.php
@@ -8,7 +8,10 @@
   I'm not so certain what the inner loop might be, but we are getting closer all the time.
   Here is another test from VS Code to GitHub to OpenShift. <br/>
   This time via commandline. <br/>
-  This time via WebHook.
+  This time via WebHook.  <br/>
+  How might we get a configMap or Secret value? <br/>
+  <?php print_r(parse_ini_file("test.ini")); ?> <br />
+  Did it work?
  <?php phpinfo(); ?>
  </body>
 </html>
